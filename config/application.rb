76326a638a8c0814a0d725c103c88f3f2155293a
BogdanBusko <busko.bogdan@gmail.com>
2017-07-10 21:06:00 +0300

    configurate database_cleaner

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,22 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-# require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-# require "sprockets/railtie"
-require "rails/test_unit/railtie"
-require "mongoid"
+require 'active_model/railtie'
+require 'active_job/railtie'
+# require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
+require 'mongoid'
+require 'database_cleaner'
 
+DatabaseCleaner.strategy = :truncation
+
+#Load  mongoid configuration file
 Mongoid.load!('config/mongoid.yml')
 
 # Require the gems listed in Gemfile, including any gems
